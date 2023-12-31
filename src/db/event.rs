77bbe9ecc92d6942a0a84b42345ff0280494014a
ThomasAqu1nas <andranikoganyan27@gmail.com>
2023-12-31 17:26:52 +0700

    feat: add new services

--- src/db/event.rs
+++ src/db/event.rs
@@ -1,8 +1,14 @@
-use sqlx::postgres::PgQueryResult;
+use chrono::{DateTime, Utc};
+use sqlx::{postgres::PgQueryResult, Postgres, QueryBuilder, Execute};
 use uuid::Uuid;
 
-use crate::{models::{Event, User}, PGPool};
+use crate::{models::{Event, User}, PGPool, dto};
 
+pub enum Filter{
+    DT((DateTime<Utc>, DateTime<Utc>)),
+    PLACE(String),
+    CREATOR(Uuid)
+}
 
 pub async fn create(event: Event, pool: &PGPool) -> Result<PgQueryResult, sqlx::Error> {
     let res = sqlx::query_as!(Event, "INSERT INTO events (id, title, descr, dt, place, creator) 
@@ -44,6 +50,65 @@ pub async fn get_participants(id: Uuid, pool: &PGPool) -> Result<Vec<User>, sqlx
     ).fetch_all(pool)
     .await;
     res
+}
+
+pub async fn set_fields<'a>(id: Uuid, event_fields: dto::UpdateEventDto, pool: &'a PGPool) -> Result<u64, sqlx::Error> {
+    let fields: Option<Vec<(String, String)>> = event_fields.get_values();
+    match fields {
+        Some(v) => {
+            let mut query_builder: QueryBuilder<Postgres> = QueryBuilder::new(
+                "UPDATE events SET "
+            );
+            let mut separated = query_builder.separated(", ");
+            for field in v {
+                separated.push_bind(format!("{:?} = {:?}", field.0, field.1));
+            }
+            separated.push_unseparated(format!("WHERE id = {id}"));
+
+            let query = query_builder.build();
+            let sql = query.sql();
+            println!("function 'set_fields' was executed with sql query string '{:}'", sql);
+            let res = sqlx::query(sql)
+                .execute(pool)
+                .await;
+            match res {
+                Ok(val) => Ok(val.rows_affected()),
+                Err(err) => Err(err)
+            }
+        },
+        None => Ok(0u64)
+        
+    }
+}
+
+pub async fn filter<'a>(filters: Filter, pool: &PGPool) -> Result<Vec<Event>, sqlx::Error> {
+    match filters {
+        Filter::DT(datetime) => {
+            sqlx::query_as!(
+                Event,
+                "SELECT * FROM events WHERE dt >= $1 AND dt < $2",
+                datetime.0,
+                datetime.1
+            ).fetch_all(pool)
+            .await
+        },
+        Filter::PLACE(place) => {
+            sqlx::query_as!(
+                Event,
+                "SELECT * FROM events WHERE place = $1",
+                place
+            ).fetch_all(pool)
+            .await
+        },
+        Filter::CREATOR(creator_id) => {
+            sqlx::query_as!(
+                Event,
+                "SELECT * FROM events WHERE creator = $1",
+                creator_id
+            ).fetch_all(pool)
+            .await  
+        },
+    }
 }
 
 
