4d10bea961df8e79d301781e4cab1c4b36e49b53
ThomasAqu1nas <andranikoganyan27@gmail.com>
2024-01-06 19:01:49 +0700

    fix: bringing logic to the service layer

--- src/db/invitations.rs
+++ src/db/invitations.rs
@@ -1,46 +1,25 @@
 use sqlx;
-use uuid::Uuid;
 
 use crate::{PGPool, models::Invitation};
 
 pub async fn create(
-   event_id: Uuid, 
-   user_id: Uuid, 
+   invitation: Invitation,
    pool: &PGPool
 ) -> Result<u64, sqlx::Error>{
-   let user_exists = super::user::exists_by_id(user_id.clone(), pool).await;
-   let event_exists = super::event::exists(event_id.clone(), pool).await;
-   let link = create_invitation_link(&event_id);
-   let invitation_id = Uuid::new_v4();
-   let invitation = Invitation {
-    id: invitation_id,
-    event_id,
-    user_id,
-    link: Some(link),
-   };
-   if user_exists && event_exists {
-      let res: Result<sqlx::postgres::PgQueryResult, sqlx::Error> = sqlx::query_as!(
-         Invitation, 
-         "INSERT INTO invitations (id, event_id, user_id, link) 
-         VALUES ($1, $2, $3, $4)",
-         invitation.id, invitation.event_id, invitation.user_id, invitation.link
-      ).execute(pool)
-      .await;
-      let notification_id = notifications::create(&invitation, pool)
-      .await?;
-      notifications::update_status_send(&notification_id, pool).await?;
-      match res {
-         Ok(rows_affected) => Ok(rows_affected.rows_affected()),
-         Err(err) => Err(err)
-      }
-
-   } else {
-      return Err(sqlx::Error::RowNotFound)
+   let res: Result<sqlx::postgres::PgQueryResult, sqlx::Error> = sqlx::query_as!(
+      Invitation, 
+      "INSERT INTO invitations (id, event_id, user_id, link) 
+      VALUES ($1, $2, $3, $4)",
+      invitation.id, invitation.event_id, invitation.user_id, invitation.link
+   ).execute(pool)
+   .await;
+   let notification_id = notifications::create(&invitation, pool)
+   .await?;
+   notifications::update_status_send(&notification_id, pool).await?;
+   match res {
+      Ok(rows_affected) => Ok(rows_affected.rows_affected()),
+      Err(err) => Err(err)
    }
-}
-
-pub fn create_invitation_link(event_id: &Uuid) -> String {
-   format!("http://127.0.0.1:8080/accept-invitaion/{:?}", *event_id)
 }
 
 pub mod notifications {
